e26ae3d161ad6d07a0bdfc4ad5a7879fc9e05f4e
Moniica2505 <55800480+Moniica2505@users.noreply.github.com>
2021-02-05 11:57:15 +0100

    Add files via upload

--- Recetas/app/build/generated/source/buildConfig/debug/com/example/recetas/BuildConfig.java
+++ Recetas/app/build/generated/source/buildConfig/debug/com/example/recetas/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.example.recetas.test;
+package com.example.recetas;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.recetas.test";
+  public static final String APPLICATION_ID = "com.example.recetas";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
